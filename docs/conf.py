e4ce111e71da2685fb4497cd5cdb9b80eb435965
xmayeur <xavier@mayeur.be>
2024-06-27 13:16:06 +0200

    upd .readthedocs.yaml + requirements.tx in docs

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,7 @@ sys.path.insert(0, os.path.abspath('../src/pyArchimate/writers'))
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
